ab11e0699bd2bb90aadcfacbbd7c251e7eb51daf
Amirirun <N.Amiri.inf24@stud.bbbaden.ch>
2024-11-08 11:40:47 +0100

    Add files via upload

---
+++ Kalorienrechner.cs
@@ -0,0 +1,206 @@
+﻿    string gewicht;
+    double gewicht1;
+
+
+    string grösse;
+    int grösse1;
+
+    string alter;
+    int alter1;
+
+    string aktiv;
+    double aktiv1;
+
+    string ziel;
+    int ziel1;
+
+    double ergebnis = 0;
+
+    Console.WriteLine("Wilkommen zum BMI rechner");
+    while (true)
+    {
+        Console.WriteLine("Gebe dein Gewicht ein:");
+        gewicht = Console.ReadLine();
+
+        if (double.TryParse(gewicht, out gewicht1))
+        {
+            if (gewicht1 > 0 && gewicht1 < 250)
+            {
+
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Deine Eingabe ist ungültig. Bitte gib ein Gewicht zwischen 1 und 250 ein.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ungültige Eingabe. Bitte gib eine Zahl ein.");
+
+        }
+    }
+    Console.WriteLine("-----------------------------------------------------------------------------");
+
+
+    while (true)
+    {
+        Console.WriteLine("Gebe deine Größe in cm ein:");
+        grösse = Console.ReadLine();
+
+    
+        if (int.TryParse(grösse, out grösse1))
+        {
+        
+            if (grösse1 > 25 && grösse1 < 220)
+            {
+            
+                break;
+            }
+            else
+            {
+          
+                Console.WriteLine("Deine Eingabe ist ungültig. Bitte gib eine Größe zwischen 26 und 219 cm ein.");
+            }
+        }
+        else
+        {
+        
+            Console.WriteLine("Ungültige Eingabe. Bitte gib eine gültige Zahl ein.");
+        }
+    }
+
+    Console.WriteLine("-----------------------------------------------------------------------------");
+
+
+    while (true)
+    {
+        Console.WriteLine("Gebe dein Alter ein:");
+        alter = Console.ReadLine();
+
+        if (int.TryParse(alter, out alter1))
+        {
+       
+            if (alter1 > 0 && alter1 < 100)
+            {
+            
+                break;
+            }
+            else
+            {
+            
+                Console.WriteLine("Deine Eingabe ist ungültig. Bitte gib ein Alter zwischen 1 und 99 ein.");
+            }
+        }
+        else
+        {
+        
+            Console.WriteLine("Ungültige Eingabe. Bitte gib eine gültige Zahl ein.");
+        }
+    }
+
+
+    Console.WriteLine("-----------------------------------------------------------------------------");
+
+    while (true)
+    {
+        Console.WriteLine("Wie aktiv bist du (1, 2, 3, 4, 5):");
+        Console.WriteLine("1. Sitzend (wenig Bewegung)");
+        Console.WriteLine("2. Leicht aktiv (Sport an 1-3 Tagen)");
+        Console.WriteLine("3. Moderat aktiv (Sport an 3-5 Tagen)");
+        Console.WriteLine("4. Sehr aktiv (Sport an 6-7 Tagen)");
+        Console.WriteLine("5. Extrem aktiv (Hochleistungssportler)");
+
+        aktiv = Console.ReadLine();
+
+    
+        if (double.TryParse(aktiv, out aktiv1) && aktiv1 >= 1 && aktiv1 <= 5)
+        {
+        
+            switch (aktiv1)
+            {
+            case 1:
+                aktiv1 = 1.2;
+                break;
+            case 2:
+                aktiv1 = 1.375;
+                break;
+            case 3:
+                aktiv1 = 1.55;
+                break;
+            case 4:
+                aktiv1 = 1.725;
+                break;
+            case 5:
+                aktiv1 = 1.9;
+                break;
+        }
+
+
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ungültige Eingabe. Bitte gib eine Zahl zwischen 1 und 5 ein.");
+        }
+    
+    }
+
+    Console.WriteLine("-----------------------------------------------------------------------------");
+
+    Console.WriteLine("Was ist dein Ziel (1, 2, 3)");
+    Console.WriteLine("1.Abnehmen");
+    Console.WriteLine("2.Gewicht halten");
+    Console.WriteLine("3.Zunehmen");
+    while (true)
+    {
+        ziel = Console.ReadLine();
+
+        if (int.TryParse(ziel, out ziel1))
+        {
+            switch (ziel1)
+
+            {
+                case 1: ziel1 -= 501;
+                    break;
+
+                case 2: ziel1 -= 2;
+                    break;
+
+
+                case 3: ziel1 += 497;
+                    break;
+            }
+
+            break;
+        }
+
+        else
+        {
+            Console.WriteLine("Ungültige Eingabe. Bitte gib eine Zahl zwischen 1 und 3 ein.");
+        }
+
+    }
+
+
+
+
+    ergebnis = ((10 * gewicht1) + (6.25 * grösse1) - (5 * alter1)) * aktiv1 + ziel1;
+
+
+    Console.WriteLine($"Dein Kalorienbedarf beträgt: {ergebnis}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ schere, stein, papier v.2.cs
@@ -0,0 +1,138 @@
+﻿
+
+
+
+
+string eingabe;
+string eingabe1;
+int punktzahl = 0;
+Console.WriteLine("Wilkommen bei Schere Stein Papier");
+Console.WriteLine("Willst du die Regeln wissen [y, n]?");
+
+while (true)
+{
+    eingabe = Console.ReadLine();
+
+    if (eingabe == "y")
+    {
+        Console.WriteLine("Die Regeln lauten:");
+        Console.WriteLine("1. Der Benutzer hat eine Auswahl von Schere, Stein und Papier");
+        Console.WriteLine("2. Für jede gewonne Runde gibt es einen Punkt");
+        Console.WriteLine("3. Für jede verlorene Runde wird ein Punkt abgezogen");
+        Console.WriteLine("4. Unentschieden wird wiederholt");
+        Console.WriteLine("5. Stein gewinnt gegen Schere");
+        Console.WriteLine("6. Schere gewinnt gegen Papier");
+        Console.WriteLine("7. Papier gewinnt gegen Stein");
+        break;
+    }
+    else if (eingabe == "n")
+    {
+        Console.WriteLine("Alles klar, zeig dein Können!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Gebe [y, n] ein");
+    }
+}
+
+Console.WriteLine("--------------------------------------------");
+
+while (true)
+{
+    Random random = new Random();
+    int zufallszahl = random.Next(1, 4);
+
+    string zahl;
+    int zahl1 = 0;
+
+    Console.WriteLine("Gebe entweder [1] für Schere, [2] für Stein oder [3] für Papier ein");
+
+    while (true)
+    {
+        zahl = Console.ReadLine();
+
+        if (int.TryParse(zahl, out zahl1))
+        {
+           switch (zahl1)
+            {
+                case 1: Console.WriteLine("Du: Schere");
+                        break;
+
+                case 2: Console.WriteLine("Du: Stein");
+                        break;
+
+                case 3: Console.WriteLine("Du: Papier");
+                        break;
+
+                default: Console.WriteLine("Deine Zahl muss zwischen 1 und 3 liegen");
+                         continue;
+
+
+
+            }
+
+
+            break;
+            
+   
+        }
+    }
+
+    Console.WriteLine("vs");
+
+    switch ( zufallszahl)
+    {
+        case 1:
+            Console.WriteLine("Dein Gegner: Schere");
+            break;
+
+        case 2:
+            Console.WriteLine("Dein Gegner: Stein");
+            break;
+
+        case 3:
+            Console.WriteLine("Dein Gegner: Papier");
+            break;
+
+
+
+
+
+
+
+    }
+
+    if (zahl1 == zufallszahl)
+    {
+        Console.WriteLine("Knappes Duell, aber unentschieden");
+    }
+    else if ((zahl1 == 1 && zufallszahl == 3) || // Schere schlägt Papier
+             (zahl1 == 2 && zufallszahl == 1) || // Stein schlägt Schere
+             (zahl1 == 3 && zufallszahl == 2))   // Papier schlägt Stein
+    {
+        punktzahl += 1;
+        Console.WriteLine("Du hast gewonnen");
+    }
+    else
+    {
+        punktzahl -= 1;
+        Console.WriteLine("Du hast verloren");
+    }
+
+    
+    Console.WriteLine("Drücke [e] um erneut zu spielen oder [q] zum Beenden");
+    eingabe1 = Console.ReadLine();
+
+    if (eingabe1 == "q")
+    {
+        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine($"Endgültige Punktzahl: {punktzahl}");
+        Console.WriteLine("--------------------------------------------");
+        break; // Beendet das Spiel
+    }
+    else if (eingabe1 != "e")
+    {
+        Console.WriteLine("Ungültige Eingabe. Drücke [e] zum Weiterspielen oder [q] zum Beenden.");
+    }
+}
